d30d8d165718f1a578652305ba233572dfeb2845
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-06-30 19:23:59 -0400

    Initial commits. Added some sample data and model training script.

---
+++ backend/train_model.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+import joblib
+
+df = pd.read_csv('../data/cars.csv')
+
+x = df[['year', 'mileage', 'msrp']]
+y = df[['current_value']]
+
+model = LinearRegression()
+model.fit(x, y)
+
+joblib.dump(model, 'model.pkl')
+print('Model trained and saved as model.pkl')
